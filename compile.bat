26960c24242db31951d96ccad437684a152d990d
wzhxyz <wzh2012@outlook.com>
2020-03-20 11:02:53 +0800

    优化目录结构

--- compile.bat
+++ compile.bat
@@ -1,6 +1,9 @@
-nasm -I include/ mdr.asm -o boot.bin 
-nasm -I include/ loader.asm -o loader.bin
-
+@echo off
+cd boot
+nasm -I include/ mdr.asm -o ../productions/boot.bin 
+nasm -I include/ loader.asm -o ../productions/loader.bin
+cd ..
+cd productions
 dd if=boot.bin of=hd0.img bs=512 count=1 conv=notrunc
 dd if=loader.bin of=hd0.img bs=512 count=10 seek=2 conv=notrunc
 if "%1" == "b" (
@@ -10,5 +13,6 @@ else if "%1" == "d" (
   bochsdbg -f bochsrc
 )^
 else (
-  ..\qemu\qemu-system-x86_64.exe  -drive file=hd0.img,media=disk,index=0,format=raw
+  qemu-system-x86_64.exe  -drive file=hd0.img,media=disk,index=0,format=raw
 ) 
+cd ..
